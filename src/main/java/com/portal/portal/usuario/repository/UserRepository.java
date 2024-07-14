948417ee4ccd484dd83994f933e75af11fc96618
Duvan Montoya <dubis310@gmail.com>
2024-07-14 08:47:31 -0500

    Se corrigen todos los errores que son necesarios

--- src/main/java/com/portal/portal/usuario/repository/UserRepository.java
+++ src/main/java/com/portal/portal/usuario/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.portal.portal.usuario.repository;
 
+import com.portal.portal.usuario.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<Integer, User> {
+
 }
