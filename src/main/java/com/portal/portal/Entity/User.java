f3c7923b0ea549b83d19d317c37a6697a847dd5f
juan montoya <piperivas77@gmail.com>
2024-07-13 22:50:09 -0500

    se crea un entity y su cuerpo

--- src/main/java/com/portal/portal/Entity/User.java
+++ src/main/java/com/portal/portal/Entity/User.java
@@ -1,8 +1,22 @@
 package com.portal.portal.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
 public class User {
+    @Id
     private int id;
     private String name;
     private String emali;
-    private String adrres;
+    private String addrres;
 }
